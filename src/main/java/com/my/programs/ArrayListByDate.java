0e90f8a7ff26abeb88a1d7b8c7b3f759df4b5fe1
Devi Vegesna <devivegesna@Devis-Mac-mini.local>
2024-08-12 15:13:38 +0530

    hi

--- src/main/java/com/my/programs/ArrayListByDate.java
+++ src/main/java/com/my/programs/ArrayListByDate.java
@@ -1,7 +1,38 @@
+//Help Saransh in writing a java program to sort objects in array list by dates.
+
 package com.my.programs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+class DateItem{
+    String date;
+
+    public DateItem(String date) {
+        this.date = date;
+    }
+}
+
+
+class sortItem implements Comparator<DateItem> {
+    public int compare (DateItem a, DateItem b){
+        return a.date.compareTo(b.date);
+    }
+}
 
 public class ArrayListByDate {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        ArrayList<DateItem> dateList = new ArrayList<>();
+        dateList.add(new DateItem("2020-03-25"));
+        dateList.add(new DateItem("2019-01-27"));
+        dateList.add(new DateItem("1998-01-27"));
+        dateList.add(new DateItem("1998-02-26"));
+        Collections.sort(dateList,new sortItem());
+        System.out.println("Sorted in ascending order");
+        for (DateItem d : dateList){
+            System.out.println(d.date);
+        }
     }
 }
